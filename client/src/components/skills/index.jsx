8386a1f251defa56ceeafba27759d0346329da64
jradmorrison <142756649+jradmorrison@users.noreply.github.com>
2024-01-17 11:13:39 -0500

    updated content of skill section

--- client/src/components/skills/index.jsx
+++ client/src/components/skills/index.jsx
@@ -11,29 +11,31 @@ const skills = () => {
       badge: <FaHtml5 size={80} style={{ color: '#E96228' }} />,
       title: 'HTML | CSS',
       content:
-        'I was first introduced to HTML and CSS in 2013 when I took web design in high school. After a long hiatus, I decided to turn what was a hobby into my career',
+        'In 2013, my journey into the world of web design began during high school, where I was introduced to HTML and CSS. This experience became the cornerstone of my understanding of UI/UX and front-end development. After a significant hiatus, I made the conscious decision to transform my once-cherished hobby into a fulfilling career.',
     },
     {
       id: 1,
       badge: <TbBrandJavascript size={80} style={{ color: '#FFD940' }} />,
-      title: 'JavaScript',
+      title: 'JavaScript | TypeScript',
       content:
-        "Learning JavaScript has been an amazing experience. I've enjoyed every minute and every challenge. From creating interactive and responsive user interfaces, to honing my skills on the back end with Node.js",
+        "My exploration of JavaScript has been an enriching journey. Starting from the basics, I gradually delved into the intricacies of the language, honing my skills in both front-end and back-end development. The process involved overcoming challenges, mastering new concepts, and ultimately gaining a solid understanding of how to craft dynamic and efficient web applications. I now find joy and confidence in my ability to navigate the complexities of JavaScript and TypeScript seamlessly.",
     },
     {
       id: 2,
       badge: <FaNodeJs size={80} style={{ color: '#8CC84B' }} />,
       title: 'Technologies',
       content:
-        "Of course my knowledge and skill as a full stack developer wouldn't be complete without the numerous frameworks, technologies, and databases I've learned along the way. Which include, but are not limited to MySQL, MongoDB, Express.js, React, GraphQL, Mongoose, Sequelize, and many others",
+        "Of course my knowledge and skill as a full stack developer wouldn't be complete without the numerous frameworks, technologies, and databases I've learned along the way. Which include, but are not limited to React, NextJS, MySQL, MongoDB, Node, Express, GraphQL, and many others",
     },
   ];
 
   return (
     <div id='skills'>
-      <div className='w-100 border-0' style={{height: '3rem', backgroundColor: 'whitesmoke'}}></div>
+      <div
+        className='w-100 border-0'
+        style={{ height: '3rem', backgroundColor: 'whitesmoke' }}></div>
       <h2 className='py-5 text-center text-white title'>SKILL OVERVIEW</h2>
-      <div className='container d-flex flex-wrap text-white text-center py-5'>
+      <div className='container d-flex flex-wrap text-white py-5'>
         {skillData.map((skill) => (
           <SkillPartial key={skill.id} skill={skill} />
         ))}
--- client/src/components/skills/skillPartial.jsx
+++ client/src/components/skills/skillPartial.jsx
@@ -1,9 +1,9 @@
 const SkillPartial = ({ skill }) => {
   return (
-    <div className="col-lg-4 px-3">
-      <div className="mb-3">{skill.badge}</div>
-      <h3 className="mb-4">{skill.title}</h3>
-      <p className="mb-4">{skill.content}</p>
+    <div className="col-lg-4">
+      <div className="mb-3 text-center">{skill.badge}</div>
+      <h3 className="mb-4 text-center">{skill.title}</h3>
+      <p className="mb-4 px-4">{skill.content}</p>
     </div>
   );
 };
