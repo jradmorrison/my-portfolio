0072380015bb911d77019590674041d45c09d0ea
jradmorrison <142756649+jradmorrison@users.noreply.github.com>
2024-01-17 12:06:56 -0500

    improved hero title animation by adding an on-window-load event listener

--- client/src/components/heroImg/index.jsx
+++ client/src/components/heroImg/index.jsx
@@ -2,17 +2,23 @@ import './style.css';
 import { FaChevronDown } from 'react-icons/fa';
 
 const heroImg = () => {
+  window.addEventListener('load', () => {
+    const heroTitle = document.getElementById('heroTitle');
+    heroTitle.innerHTML = '// WEB DEVELOPER';
+    heroTitle.classList.add('slideIn');
+  });
+
   return (
-    <div className="hero" id='top'>
-      <div className="title slideIn container d-flex justify-content-end">
-        // WEB DEVELOPER
-      </div>
-      <div className="d-flex justify-content-center arrow">
-        <a href="#about-me">
+    <div className='hero' id='top'>
+      <div
+        id='heroTitle'
+        className='title container d-flex justify-content-end'></div>
+      <div className='d-flex justify-content-center arrow'>
+        <a href='#about-me'>
           <FaChevronDown
             size={40}
             style={{ color: '#fff' }}
-            className="bounce"
+            className='bounce'
           />
         </a>
       </div>
